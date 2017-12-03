8385964d6beb51b7901c93e91af4a70f6380899f
Cameron Sweeney <cp_sweeney@hotmail.com>
2017-12-03 15:41:36 -0800

    Initial development

---
+++ README.md
@@ -0,0 +1,12 @@
+# Term Project COMP 455
+### Check for available usernames with a Bloom Filter
+November 30, 2017<br >
+Cameron Sweeney
+
+### Install Requirements
+
+Run `pip install -r requirements.txt`
+
+Run `python bloom-filter.py`
+
+###
---
+++ bloom-filter.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# COMP 455 Term Project
+# Check available usernames with a Bloom Filter
+
+import mmh3
+import os
+
+class BloomFilter:
+
+    def __init__(self, bit_array_size, number_hashes):
+        # Initialize the bit array
+        self.bit_array_size = bit_array_size
+        self.bit_array = [0] * bit_array_size
+        self.number_hashes = number_hashes
+
+    # Add usernames into the bit array
+    def add_to_bit_array(self, username):
+
+        for seed_count in range(self.number_hashes):
+            result = mmh3.hash(username, seed_count) % self.bit_array_size
+            print("{0}'s hash value is {1}".format(username, result))
+            self.bit_array[result] = 1
+
+    # Check if the username is in the bit_array
+    def check_availability(self, username):
+
+        for seed_count in range(self.number_hashes):
+
+            # Calculate hash on given username
+            given_username = mmh3.hash(username, seed_count) % self.bit_array_size
+
+        if self.bit_array[given_username] == 0:
+            print("Username {0} is available!".format(username))
+        else:
+            print("Username {0} not available.".format(username))
+
+# Create bloom filter object
+bloom_filter = BloomFilter(100, 1)
+
+# Open the file with the usernames
+file_path = os.path.dirname(os.path.realpath(__file__)) + "/names/test-names.txt"
+
+# Read in the names from the txt file line by line
+with open(file_path) as unavailable_usernames:
+    for name in unavailable_usernames:
+        bloom_filter.add_to_bit_array(name.rstrip('\n'))
+
+
+
+bloom_filter.check_availability("Abagael")
+bloom_filter.check_availability("Cameron")
+bloom_filter.check_availability("Cam")
